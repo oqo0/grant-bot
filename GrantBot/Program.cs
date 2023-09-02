8cc156e2e208ac297efa5e1ee32593cebe5a0cb1
oqo0 <oqqqqqqqqqqq@gmail.com>
2023-09-02 10:40:42 +0300

    Add project template

--- GrantBot/Program.cs
+++ GrantBot/Program.cs
@@ -1,3 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Hello, World!");
+namespace GrantBot;
+
+public class Program
+{
+    private readonly IConfiguration _configuration;
+    private readonly IServiceProvider _services;
+
+    private readonly DiscordSocketConfig _socketConfig = new()
+    {
+        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+        AlwaysDownloadUsers = true,
+    };
+
+    private Program()
+    {
+        _configuration = new ConfigurationBuilder()
+            .AddJsonFile("settings.json", optional: true)
+            .Build();
+
+        _services = new ServiceCollection()
+            .AddSingleton(_configuration)
+            .AddSingleton(_socketConfig)
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+            .AddSingleton<InteractionHandler>()
+            .BuildServiceProvider();
+    }
+
+    static void Main(string[] args)
+        => new Program()
+            .RunAsync()
+            .GetAwaiter()
+            .GetResult();
+
+    private async Task RunAsync()
+    {
+        var client = _services.GetRequiredService<DiscordSocketClient>();
+
+        client.Log += LogAsync;
+
+        await _services.GetRequiredService<InteractionHandler>()
+            .InitializeAsync();
+
+        await client.LoginAsync(TokenType.Bot, _configuration["botToken"]);
+        await client.StartAsync();
+
+        await Task.Delay(Timeout.Infinite);
+    }
+
+    private Task LogAsync(LogMessage message)
+    {
+        Console.WriteLine(message.ToString());
+        return Task.CompletedTask;
+    }
+
+    public static bool IsDebug()
+    {
+        #if DEBUG
+            return true;
+        #else
+            return false;
+        #endif
+    }
+}
