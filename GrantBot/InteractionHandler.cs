dd9a1159a94a4ddb37eaf7e92668598c73c9b9fb
oqo0 <oqqqqqqqqqqq@gmail.com>
2023-09-08 12:01:12 +0300

    Remove redundant comments & refactor

--- GrantBot/InteractionHandler.cs
+++ GrantBot/InteractionHandler.cs
@@ -23,14 +23,11 @@ public class InteractionHandler
 
     public async Task InitializeAsync()
     {
-        // Process when the client is ready, so we can register our commands.
         _client.Ready += ReadyAsync;
         _handler.Log += LogAsync;
 
-        // Add the public modules that inherit InteractionModuleBase<T> to the InteractionService
         await _handler.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
 
-        // Process the InteractionCreated payloads to execute Interactions commands
         _client.InteractionCreated += HandleInteraction;
     }
 
@@ -47,10 +44,8 @@ public class InteractionHandler
     {
         try
         {
-            // Create an execution context that matches the generic type parameter of your InteractionModuleBase<T> modules.
             var context = new SocketInteractionContext(_client, interaction);
 
-            // Execute the incoming command.
             var result = await _handler.ExecuteCommandAsync(context, _services);
 
             if (!result.IsSuccess)
@@ -58,20 +53,34 @@ public class InteractionHandler
                 switch (result.Error)
                 {
                     case InteractionCommandError.UnmetPrecondition:
-                        // implement
+                        // TO-DO
+                        break;
+                    case InteractionCommandError.Exception:
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        break;
+                    case InteractionCommandError.ConvertFailed:
+                        break;
+                    case InteractionCommandError.ParseFailed:
+                        break;
+                    case InteractionCommandError.UnknownCommand:
                         break;
                     default:
                         break;
                 }
             }
         }
-        catch
+        catch (Exception)
         {
-            // If Slash Command execution fails it is most likely that the original interaction acknowledgement will persist. It is a good idea to delete the original
-            // response, or at least let the user know that something went wrong during the command execution.
             if (interaction.Type is InteractionType.ApplicationCommand)
+            {
                 await interaction.GetOriginalResponseAsync()
                     .ContinueWith(async msg=> await msg.Result.DeleteAsync());
+
+                await interaction.FollowupAsync("An error occurred during the execution of the module.");
+            }
         }
     }
 }
