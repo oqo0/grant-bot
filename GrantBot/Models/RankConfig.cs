c458904a51877e4ed18ab8769b5bdf7f7aa6bf72
oqo0 <oqqqqqqqqqqq@gmail.com>
2023-09-19 16:15:57 +0300

    Add config alias members

--- GrantBot/Models/RankConfig.cs
+++ GrantBot/Models/RankConfig.cs
@@ -4,8 +4,15 @@ namespace GrantBot.Models;
 
 public class RankConfig
 {
+    [YamlMember(Alias = "name")]
     public string Name { get; set; }
+    
+    [YamlMember(Alias = "id")]
     public ulong Id { get; set; }
+    
+    [YamlMember(Alias = "priority")]
     public uint Priority { get; set; }
+    
+    [YamlMember(Alias = "image-path")]
     public string? Image { get; set; }
 }
