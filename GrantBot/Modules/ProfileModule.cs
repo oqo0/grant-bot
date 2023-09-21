c17435e423e412ce1e65de62ed4bf749ceedb579
oqo0 <oqqqqqqqqqqq@gmail.com>
2023-09-21 15:55:45 +0300

    Implement profile slash command

---
+++ GrantBot/Modules/ProfileModule.cs
@@ -0,0 +1,52 @@
+using System.ComponentModel;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using GrantBot.AutocompleteHandlers;
+using GrantBot.Data.Models;
+using GrantBot.Data.Repositories;
+using GrantBot.Services.Painters;
+using GrantBot.Utils.TypeConverters;
+using SixLabors.ImageSharp.Formats.Png;
+using SmartFormat;
+
+namespace GrantBot.Modules;
+
+[RequireContext(ContextType.Guild)]
+[EnabledInDm(false)]
+public class ProfileModule : InteractionModuleBase<SocketInteractionContext>
+{
+    private readonly IProfileInfoPainter _profileInfoPainter;
+    private readonly IUserRepository _userRepository;
+
+    public ProfileModule(
+        IProfileInfoPainter profileInfoPainter,
+        IUserRepository userRepository)
+    {
+        _profileInfoPainter = profileInfoPainter;
+        _userRepository = userRepository;
+    }
+
+    [SlashCommand("profile", "Show user profile information.")]
+    public async Task Profile(
+        SocketGuildUser guildUser,
+        [Autocomplete(typeof(SeasonAutocompleteHandler)),
+         TypeConverter(typeof(SeasonTypeConverter))] Season season)
+    {
+        var user = _userRepository.GetByDiscordId(guildUser.Id);
+        
+        if (user is null)
+        {
+            await RespondAsync("not found");
+            return;
+        }
+                
+        using var stream = new MemoryStream();
+        var awardReceivedImage = _profileInfoPainter.Draw(user, season.Id);
+        await awardReceivedImage.SaveAsync(stream, new PngEncoder());
+    
+        await RespondWithFileAsync(
+            new FileAttachment(stream, "userProfile.png"),
+            "This is an image");
+    }
+}
